e77b496c8fece1b889b2b541119ee3b1a8f6a72b
Nikita Yudin <metalworldowner@gmail.com>
2021-11-19 12:43:28 +0300

    add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ApplicationRecord
 
-validates :email presence: true
+
+VALID_EMAIL_REGEX = /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
+
+validates :email presence: true, 
+								uniqueness: { case_sensitive: false }, 
+								length: { maximum: 108, minimum: 8}, 
+								format: { with: VALID_EMAIL_REGEX }
 
 					:password presence: true
 
-					:username presence: true
-
+					:username presence: true, 
+										uniqueness: { case_sensitive: false }
 
 end
