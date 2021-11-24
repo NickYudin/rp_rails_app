40edefaafd3d399308e1b041d6a2fb9690e9cd41
Nikita Yudin <metalworldowner@gmail.com>
2021-11-24 13:36:03 +0300

    add users edit

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 	get 'pages/index'
 	resources :battles, :battlers, :rounds
 	get 'signup', to: 'users#new'
-	post 'users', to: 'users#create'
+	resources :users, :except => [:new]
 
 end
